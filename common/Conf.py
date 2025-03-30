8ba673bb8fd8883ff722686c6689fdbb820d1cd9
Daniel Escalante Perez <d.escalante@uniandes.edu.co>
2025-03-30 14:46:54 -0500

    Using openvino

--- common/Conf.py
+++ common/Conf.py
@@ -2,7 +2,7 @@ import cv2
 
 class Conf():
 
-    def __init__(self, skip_frames, model, camera, confidence, distance = 100, font = cv2.FONT_HERSHEY_SIMPLEX, font_size = 1, font_color = (0, 255, 0), font_thickness = 5, backgrounds = (255, 0, 0), boxes = (0, 0, 255), label_offset = 50, fps = 30, show_camera = True):
+    def __init__(self, skip_frames, model, camera, confidence, distance = 100, font = cv2.FONT_HERSHEY_SIMPLEX, font_size = 1, font_color = (0, 255, 0), font_thickness = 5, backgrounds = (255, 0, 0), boxes = (0, 0, 255), label_offset = 50, fps = 30, show_camera = True, width = 320, height = 240):
         self.camera = camera
         self.distance = distance
         self.skip_frames = skip_frames
@@ -17,3 +17,5 @@ class Conf():
         self.label_offset = label_offset,
         self.fps = fps,
         self.show_camera = show_camera
+        self.width = width
+        self.height = height
--- option_2_optimized.py
+++ option_2_optimized.py
@@ -2,7 +2,13 @@ import cv2
 import pandas as pd
 from ultralytics import YOLO
 from common import Conf
+import torch
+from openvino.runtime import Core
+import os
+import numpy as np
 
+
+torch.set_num_threads(4)
 cv2.setUseOptimized(True)
 cv2.setNumThreads(4)
 
@@ -33,14 +39,19 @@ def paint(frame, pd_frame):
 def start_model_camera():
     cap = cv2.VideoCapture(conf.camera)
 
-    model=YOLO(conf.model)
+    model = None
+    if not os.path.exists(conf.model + "_openvino_model/"):
+        model=YOLO(conf.model + ".pt")
+        model.export(format="openvino") 
+    
+    model = YOLO(conf.model + "_openvino_model/")
 
     if not cap.isOpened():
         print("Error: Could not open webcam.")
         exit()
 
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, conf.width)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, conf.width)
     cap.set(cv2.CAP_PROP_FPS, 15)
 
     return model, cap
@@ -66,16 +77,14 @@ def main():
         if conf.skip_frames != 0 and count % conf.skip_frames != 0:
             continue
 
-        #frame = cv2.resize(frame, (320, 240))
 
-        results=model.predict(frame, verbose = False)
+        results = model(frame)
         
         pd_frame = get_coords_conf(results)
 
-        print(pd_frame.shape[0], " personas")
+
+        print(len(pd_frame), "personas")
         #paint(frame, pd_frame)
-        
-        #cv2.imshow("Video", frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -86,8 +95,10 @@ def main():
 model_ref = input("Insert the Ultralytics model string: ")
 skip_frames = int(input("Pick a number of frames to skip: "))
 fps = int(input("Pick a number of fps for the camera: "))
+width = int(input("Pick a width for the camera: "))
+height = int(input("Pick a height for the camera: "))
 show_camera = True if input("Show camera? (1,0): ") == "1" else False
 
-conf = Conf(show_camera = show_camera, fps = fps, camera = 0, distance = 120, skip_frames = skip_frames, model = model_ref, confidence = 0.5, font_color = (255, 255, 255), font_thickness = 3)
+conf = Conf(width = width, height = height, show_camera = show_camera, fps = fps, camera = 0, distance = 120, skip_frames = skip_frames, model = model_ref, confidence = 0.5, font_color = (255, 255, 255), font_thickness = 3)
 
 main()
